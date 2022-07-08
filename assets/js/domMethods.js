1c4e64149ce4a475b50101c4c7daf9a4443cdd5c
Megan Gedde <hoglu070@d.umn.edu>
2022-07-07 20:13:04 -0500

    optomized pics

---
+++ assets/js/domMethods.js
@@ -0,0 +1,6 @@
+function createEl(htmlString, attrs, ...children) {
+    if (typeof htmlString !== "string") {
+      throw Error("Argument 'htmlString' is required and must be a string");
+    }};
+    
+module.exports = createEl;
